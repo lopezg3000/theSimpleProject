c773c71912d1ec7c90569dd42812c1dfc654b638
Gabriel Lopez <lopezg3000@gmail.com>
2022-12-20 08:15:27 -0600

    models updated to include more fields

--- discipline_logger/models.py
+++ discipline_logger/models.py
@@ -1,27 +1,32 @@
 from django.db import models
+from login.models import User
 
 
 class Student(models.Model):
-    first_name: models.CharField(max_length=255)
-    last_name: models.CharField(max_length=255)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.first_name
 
 
 class Guardian(models.Model):
-    first_name: models.CharField(max_length=255)
-    last_name: models.CharField(max_length=255)
-    email: models.EmailField(max_length=254)
-    phone_number: models.CharField(max_length=12)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField(max_length=254)
+    phone_number = models.CharField(max_length=12)
     student = models.ForeignKey(Student, on_delete=models.CASCADE)
 
 
 class Offense(models.Model):
-    date: models.DateTimeField('date of offense')
-    offense_code: models.CharField(max_length=5)
-    offense_description: models.CharField(max_length=255)
+    date = models.DateTimeField('date of offense')
+    offense_code = models.CharField(max_length=5)
+    offense_description = models.CharField(max_length=255)
     student = models.ForeignKey(Student, on_delete=models.CASCADE)
 
 
 class Action(models.Model):
-    action_code: models.CharField(max_length=5)
-    action_description: models.CharField(max_length=255)
+    action_code = models.CharField(max_length=5)
+    action_description = models.CharField(max_length=255)
     offense = models.ForeignKey(Offense, on_delete=models.CASCADE)
--- login/models.py
+++ login/models.py
@@ -1,5 +1,10 @@
 from django.db import models
 
 
-class user(models.Model):
-    username = models.CharField(max_length=200)
+class User(models.Model):
+    username = models.EmailField(max_length=200)
+    password = models.CharField(max_length=255, default="test")
+    date_created = models.DateTimeField('date created')
+
+    def __str__(self):
+        return self.username
